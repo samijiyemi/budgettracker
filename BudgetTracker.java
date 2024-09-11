e1de9c98623b1304090048f51e3c4f5cfa0d1f2b
ogbenisamu <samijiyemi@gmail.com>
2024-09-11 16:09:38 +0100

    remove unneccesary files

--- BudgetTracker.java
+++ BudgetTracker.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class BudgetTracker {
     public static void main(String[] args) {
         System.out.println("Budget Once, Spend Wisely!!!");
-
         // create an instance of scanner for accepting input from the user in the
         // terminal
         Scanner scanner = new Scanner(System.in);
@@ -21,17 +20,14 @@ public class BudgetTracker {
 
         System.out.println("How much do you budget for education?");
         double educationBills = scanner.nextDouble();
-        // budgetPrice -= educationBills;
 
         System.out.println("How much do you budget for Miscellanous?");
         double miscBills = scanner.nextDouble();
         double totalBalancRemaining = budgetPrice - educationBills - miscBills;
 
-        // user should be able to work on the budget given in the application
-
         System.out.println("Hey, " + (name) + " you have $" + (budgetPrice) + " to spend");
         System.out.print("\nAfter Education Bills you have $" + (budgetPrice - educationBills) + " left\n");
         System.out.println("\nRemaining balance after misc have been debited: $" + (totalBalancRemaining));
-        System.out.println("Wallet Balance:" + (walletBalanceAfterBudget + totalBalancRemaining));
+        System.out.println("Wallet Balance: $" + (walletBalanceAfterBudget + totalBalancRemaining));
     }
 }
