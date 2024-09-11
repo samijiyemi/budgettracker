4a36112326fcd564c930d848471593c0a8c0a22c
ogbenisamu <samijiyemi@gmail.com>
2024-09-11 16:04:22 +0100

    initial commit

---
+++ BudgetTracker.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class BudgetTracker {
+    public static void main(String[] args) {
+        System.out.println("Budget Once, Spend Wisely!!!");
+
+        // create an instance of scanner for accepting input from the user in the
+        // terminal
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("How much is your budget?");
+        double budgetPrice = scanner.nextDouble();
+
+        System.out.println("How much do you have?");
+        double balance = scanner.nextDouble();
+
+        double walletBalanceAfterBudget = balance - budgetPrice;
+
+        System.out.println("How much do you budget for education?");
+        double educationBills = scanner.nextDouble();
+        // budgetPrice -= educationBills;
+
+        System.out.println("How much do you budget for Miscellanous?");
+        double miscBills = scanner.nextDouble();
+        double totalBalancRemaining = budgetPrice - educationBills - miscBills;
+
+        // user should be able to work on the budget given in the application
+
+        System.out.println("Hey, " + (name) + " you have $" + (budgetPrice) + " to spend");
+        System.out.print("\nAfter Education Bills you have $" + (budgetPrice - educationBills) + " left\n");
+        System.out.println("\nRemaining balance after misc have been debited: $" + (totalBalancRemaining));
+        System.out.println("Wallet Balance:" + (walletBalanceAfterBudget + totalBalancRemaining));
+    }
+}
